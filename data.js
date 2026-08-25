87ed7700e59489a0f63137d4e71e22e99e7c2d87
JhuanCarlos116 <jhuancarlos116@students.undip.ac.id>
2026-08-25 17:30:32 +0700

    RevFoto

--- data.js
+++ data.js
@@ -1,73 +1,185 @@
 // File ini DIHASILKAN otomatis oleh build.js — jangan diedit manual.
 window.LOCATIONS = [
   {
-    "name": "Dekat Gerbang UNDIP",
+    "name": "Teknik Sipil UNDIP",
+    "lat": -7.053213,
+    "lng": 110.440101,
+    "img": "photos/20260824_124516.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Bunderan UNDIP",
+    "lat": -7.055015,
+    "lng": 110.440084,
+    "img": "photos/20260824_124958.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Rusunawa UNDIP",
     "lat": -7.055095,
     "lng": 110.442716,
     "img": "photos/20260824_125138.jpg",
     "hint": ""
   },
   {
+    "name": "Stadion UNDIP",
+    "lat": -7.054763,
+    "lng": 110.445836,
+    "img": "photos/20260824_125346.jpg",
+    "hint": ""
+  },
+  {
     "name": "Waduk / Embung UNDIP",
+    "lat": -7.053454,
+    "lng": 110.446877,
+    "img": "photos/20260824_125514.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Waduk / Embung UNDIP (2)",
     "lat": -7.052582,
     "lng": 110.446185,
     "img": "photos/20260824_125628.jpg",
     "hint": ""
   },
   {
-    "name": "Toko KPRI UNDIP",
+    "name": "Rusunawa UNDIP (2)",
     "lat": -7.054871,
     "lng": 110.443446,
     "img": "photos/20260824_125851.jpg",
     "hint": ""
   },
   {
-    "name": "Gerbang Universitas Diponegoro",
+    "name": "Bunderan UNDIP (2)",
+    "lat": -7.056156,
+    "lng": 110.439298,
+    "img": "photos/20260824_130215.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Bunderan UNDIP (3)",
+    "lat": -7.05596,
+    "lng": 110.438527,
+    "img": "photos/20260824_130342.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Teknik Sipil UNDIP (2)",
+    "lat": -7.052589,
+    "lng": 110.438033,
+    "img": "photos/20260824_130627.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Dekanat Fakultas Teknik",
+    "lat": -7.051728,
+    "lng": 110.436153,
+    "img": "photos/20260824_130940.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Dekanat Fakultas Teknik (2)",
     "lat": -7.050832,
     "lng": 110.435966,
     "img": "photos/20260824_131047.jpg",
     "hint": "Koordinat perkiraan (interpolasi) - tolong koreksi jika meleset."
   },
   {
-    "name": "Gedung Fakultas UNDIP",
+    "name": "Perpustakaan UNDIP",
+    "lat": -7.049936,
+    "lng": 110.435779,
+    "img": "photos/20260824_131153.jpg",
+    "hint": ""
+  },
+  {
+    "name": "SPBU Pertamina UNDIP",
     "lat": -7.053145,
     "lng": 110.432526,
     "img": "photos/20260824_131435.jpg",
     "hint": ""
   },
   {
-    "name": "Gerbang Utama UNDIP (Widya Puraya)",
+    "name": "Perpustakaan UNDIP (2)",
+    "lat": -7.047135,
+    "lng": 110.435617,
+    "img": "photos/20260824_131635.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Perpustakaan UNDIP (3)",
+    "lat": -7.049186,
+    "lng": 110.436378,
+    "img": "photos/20260824_131733.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Widya Puraya (Rektorat)",
     "lat": -7.049358,
     "lng": 110.438458,
     "img": "photos/20260824_131909.jpg",
     "hint": ""
   },
   {
-    "name": "Gedung UNDIP Press",
+    "name": "Perpustakaan UNDIP (4)",
     "lat": -7.048549,
     "lng": 110.437946,
     "img": "photos/20260824_132015.jpg",
     "hint": ""
   },
   {
-    "name": "Magister Teknik Lingkungan UNDIP",
+    "name": "Gedung Kuliah Bersama FT",
+    "lat": -7.049711,
+    "lng": 110.439977,
+    "img": "photos/20260824_132141.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Gedung Kuliah Bersama FT (2)",
+    "lat": -7.050039,
+    "lng": 110.439821,
+    "img": "photos/20260824_132337.jpg",
+    "hint": ""
+  },
+  {
+    "name": "Gedung Kuliah Bersama FT (3)",
     "lat": -7.050837,
     "lng": 110.439859,
     "img": "photos/20260824_132504.jpg",
     "hint": ""
   },
   {
-    "name": "Waduk UNDIP (dekat FPIK)",
+    "name": "Gedung Kuliah Bersama FT (4)",
+    "lat": -7.050152,
+    "lng": 110.440592,
+    "img": "photos/20260824_132626.jpg",
+    "hint": ""
+  },
+  {
+    "name": "FPIK UNDIP",
+    "lat": -7.04902,
+    "lng": 110.441221,
+    "img": "photos/20260824_132954.jpg",
+    "hint": "Koordinat perkiraan (interpolasi) - tolong koreksi jika meleset."
+  },
+  {
+    "name": "Fak. Ekonomika & Bisnis",
     "lat": -7.047664,
     "lng": 110.441975,
     "img": "photos/20260824_133403.jpg",
     "hint": ""
   },
   {
-    "name": "Rusunawa UNDIP",
+    "name": "FPIK UNDIP (2)",
     "lat": -7.049453,
     "lng": 110.442775,
     "img": "photos/20260824_133645.jpg",
+    "hint": ""
+  },
+  {
+    "name": "FPIK UNDIP (3)",
+    "lat": -7.050114,
+    "lng": 110.441467,
+    "img": "photos/20260824_133741.jpg",
     "hint": ""
   }
 ];
